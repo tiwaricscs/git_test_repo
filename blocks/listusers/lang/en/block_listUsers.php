6730ffeda383e4b24e25de71b4805fecec0ddc5f
tiwaricscs <tiwaricscs@hotmail.com>
2020-10-09 17:53:06 +0530

    first winmoodle39 commit

---
+++ blocks/listusers/lang/en/block_listUsers.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_html', language 'en', branch 'MOODLE_20_STABLE'
+ *
+ * @package   block_listUsers
+ * @copyright 1999 onwards Martin Dougiamas  {@link http://moodle.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['listusers:addinstance'] = 'Add a new listUsers block';
+$string['listusers:myaddinstance'] = 'Add a new listUsers block to Dashboard';
+$string['newlistusersblock'] = '(new listUsers block)';
+$string['pluginname'] = 'list users';
+$string['listusers'] = 'list users';
+$string['search:content'] = 'listusers block content';
+$string['privacy:metadata:block'] = 'The listusers block stores all of its data within the block subsystem.';
